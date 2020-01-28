03dd8ac8cd718d8075c9082aacb125ddcc74d675
jordelucas <jordevalucas@hotmail.com>
2020-01-28 18:13:24 -0300

    add tela inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import './App.css'
+
+import logo from './assets/logo.svg'
 
 function App() {
   return (
-    <div id="app">
+    <div className="container">
+      <img src={logo} alt="logomarca"/>
       
+      <div className="content">
+        <h1>iTunes - Music </h1>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit
+        </p>
+
+        <button><ChevronRightIcon /></button> 
+      </div>
+
     </div>
   );
 }
