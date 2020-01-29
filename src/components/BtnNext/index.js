56efc541e7239df0f8faf4b2a174733dc1574acb
jordelucas <jordevalucas@hotmail.com>
2020-01-29 09:35:01 -0300

    add component BtnNext

---
+++ src/components/BtnNext/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+import './styles.css'
+
+function BtnPrevious() {
+    return (
+        <button className="next">  
+            <ChevronRightIcon />
+        </button>
+    )
+}
+
+export default BtnPrevious;
