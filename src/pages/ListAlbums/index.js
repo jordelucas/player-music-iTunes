a995c043fad9f1e56ec534b3be043f11749a8d16
jordelucas <jordevalucas@hotmail.com>
2020-01-29 09:36:59 -0300

    add layout listagem de albuns

--- src/pages/ListAlbums/index.js
+++ src/pages/ListAlbums/index.js
@@ -1,19 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../../services/api'
 import BtnPrevious from '../../components/BtnPrevious'
+import BtnNext from '../../components/BtnNext'
 
-// https://itunes.apple.com/lookup?id=159260351&entity=album
+import './styles.css'
 
 export default function ListAlbums({ history }) {
     const artist_id = localStorage.getItem('artist')
-    
+
+    const [albums, setAlbums] = useState([])
+    const [artist, setArtist] = useState('')
+
+    async function loadAlbums() {
+        const response = await api.get(`lookup?id=${artist_id}&entity=album&limit=15`);
+        const [name, ...array] = response.data.results;
+
+        setArtist(name);
+        setAlbums(array);
+    }
+
+    useEffect(() => {
+        loadAlbums()
+    }, [])
+
+    function splitYear(data) {
+        let [year] = data.split('-')
+        return year;
+    }
+
+
     return (
         <>
-            <div className="header">
+            <header>
                 <BtnPrevious 
                     link='/searchartist'
                     history={history}
                 />
-            </div>
+
+                <div className="title">
+                    <strong>{artist.artistName}</strong>
+                    <h1>Albums</h1>
+                </div>
+            </header>
+            <main>
+                {console.log(albums)}
+                <ul>
+                {albums.map(album => (
+                    <li key={album.collectionId} className="album-item">
+                        <img src={album.artworkUrl100} alt={album.collectionName} />
+                        
+                        <div className="album-info">
+                            <strong>{album.collectionName}</strong>
+                            <span>{splitYear(album.releaseDate)}</span>
+                        </div>
+                        
+                        <BtnNext />
+                    </li>
+                ))}
+                </ul>
+            </main>
         </>
     )
 }
